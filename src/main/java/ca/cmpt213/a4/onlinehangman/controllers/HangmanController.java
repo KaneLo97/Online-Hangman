ffef913836b9f284ec2e2883360d8117851e473b
klohogti <klohogti@sfu.ca>
2020-07-21 18:38:46 -0700

    add game id and link back to welcome page

--- src/main/java/ca/cmpt213/a4/onlinehangman/controllers/HangmanController.java
+++ src/main/java/ca/cmpt213/a4/onlinehangman/controllers/HangmanController.java
@@ -1,5 +1,6 @@
 package ca.cmpt213.a4.onlinehangman.controllers;
 
+import ca.cmpt213.a4.onlinehangman.model.Game;
 import ca.cmpt213.a4.onlinehangman.model.Message;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,9 +8,15 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class HangmanController {
     private Message promptMessage; //a resusable String object to display a prompt message at the screen
+    private AtomicLong nextId = new AtomicLong();
+    private ArrayList<Game> gameList = new ArrayList<>();
 
     //works like a constructor, but wait until dependency injection is done, so it's more like a setup
     @PostConstruct
@@ -35,8 +42,11 @@ public class HangmanController {
     }
 
     @PostMapping("/game")
-    public String showGamePage() {
-
+    public String showGamePage(Model model) {
+        Game game = new Game();
+        game.setId(nextId.incrementAndGet());
+        model.addAttribute("game", game);
+        gameList.add(game);
         return "game";
     }
 }
